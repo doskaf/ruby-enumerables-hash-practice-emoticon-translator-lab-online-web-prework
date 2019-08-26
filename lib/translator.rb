5eaed15b6669c85722999a304f7e15abace2c830
Dominique Skaf <doskaf@yahoo.com>
2019-08-26 18:49:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(file_path)
     result["get_meaning"][emoticon[1]] = meaning
     result["get_emoticon"][emoticon[0]] = emoticon[1]
   end
-result
+return result
 end
 
 def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
@@ -15,7 +15,7 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   if emote_library["get_emoticon"].include?(english_emoticon)
     emote_library["get_emoticon"][english_emoticon]
   else
-    "Sorry, that emoticon was not found"
+    puts "Sorry, that emoticon was not found"
   end
 end
 
@@ -23,5 +23,7 @@ def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
   emote_library = load_library(file_path)
   if emote_library["get_meaning"].include?(japanese_emoticon)
     emote_library["get_meaning"][japanese_emoticon]
+  else
+    puts "Sorry, that emoticon was not found"
   end
 end
