dca1c2f0be1ef09d8ed583cd201129fcd20bbfe4
Dominique Skaf <doskaf@yahoo.com>
2019-08-26 18:42:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ end
 
 def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   emote_library = load_library(file_path)
-  # code goes here
+  if emote_library["get_emoticon"].include?(english_emoticon)
 end
 
 def get_english_meaning
